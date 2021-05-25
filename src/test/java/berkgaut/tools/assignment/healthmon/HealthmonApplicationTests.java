6a3771bdbc04602754f53246e323b01a51bc782f
Boris Berkgaut <berkgaut@gmail.com>
2021-05-25 22:43:50 +0200

    ***spring initializr***

---
+++ src/test/java/berkgaut/tools/assignment/healthmon/HealthmonApplicationTests.java
@@ -0,0 +1,13 @@
+package berkgaut.tools.assignment.healthmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
